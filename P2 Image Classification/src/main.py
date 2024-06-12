44736a169e8c924e3b81d876df67769fac96d510
HKvv <823997462@qq.com>
2024-06-12 12:29:55 +0800

    init P2

---
+++ P2 Image Classification/src/main.py
@@ -0,0 +1,17 @@
+import torch
+import torchvision
+from torch.utils.data import dataloader
+
+EPOCH=100
+BATCH_SIZE=100
+
+# Data Preparation
+train_set=torchvision.datasets.CIFAR10(download=True, root='../data', train=True, 
+									   transform=torchvision.transforms.ToTensor())
+
+test_set=torchvision.datasets.CIFAR10(download=True, root='../data', train=False, 
+									  transform=torchvision.transforms.ToTensor())
+
+train_loader=dataloader(train_set, batch_size=BATCH_SIZE, shuffle=True)
+test_loader=dataloader(test_set, batch_size=BATCH_SIZE, )
+
